4ba5eb95fc1aa5d9b67094125c108923c7e557cd
Sargis Abrahamyan <31526195+sabra007@users.noreply.github.com>
2022-02-08 19:24:02 -0600

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -2,31 +2,55 @@ from ast import In
 import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, State, dcc, html
-from dash import html
 import plotly.express as px
 import pandas as pd
 import pymssql
 
 database = "group3-DB"
-table = "dbo.hstock"
+table = "dbo.rtstock"
 username = "group3_user"
 password = "K-qC4SoI_oUvepg"
 server = "gen10-data-fundamentals-21-11-sql-server.database.windows.net"
 
+conn = pymssql.connect(server, username, password, database)
 # table = "dbo.hstock"
-try:
-    conn = pymssql.connect(server, username, password, database)
+
+
+def getData(table):
+    # try:
+    #     conn = pymssql.connect(server, username, password, database)
+    # except Exception as e:
+    #     print(e)
     cursor = conn.cursor()
     query = f"SELECT * FROM {table}"
     df = pd.read_sql(query, conn)
-except Exception as e:
-    print(e)
+    return df
 
-df_aapl = df[df["Ticker"] == "AAPL"]
-fig = px.line(df_aapl, x="Date", y="Open", title="AAPL")
 
-df_msft = df[df["Ticker"] == "MSFT"]
-fig2 = px.line(df_msft, y="Close", x="Date", title=" MSFT")
+# try:
+#     conn = pymssql.connect(server, username, password, database)
+#     cursor = conn.cursor()
+#     query = f"SELECT * FROM {table}"
+#     df = pd.read_sql(query, conn)
+# except Exception as e:
+#     print(e)
+
+def createFig(table, ticker, title):
+    df = getData(table)
+    df2 = df[df["Ticker"] == ticker]
+    fig = px.line(df2, x="Timestamp", y="Open Price", title=f"{title} ({ticker})",
+                  labels={
+                      "Timestamp": "Need to convert from timestamp to time",
+                      "Open Price": "Current Price ($)"
+                      },
+                  )
+    return fig
+
+# df_aapl = df[df["Ticker"] == "AAPL"]
+# fig = px.line(df_aapl, x="Date", y="Open", title="AAPL")
+#
+# df_msft = df[df["Ticker"] == "MSFT"]
+# fig2 = px.line(df_msft, y="Close", x="Date", title=" MSFT")
 
 # -----------------------------------------------------------------------
 
@@ -54,12 +78,13 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 
+
 submenu_1 = [
     html.Li(
         # use Row and Col components to postion the chevrons
         dbc.Row(
             [
-                dbc.Col("Historical Stock Data"),
+                dbc.Col("Live Stock Data"),
                 dbc.Col(
                     html.I(className="fas fa-chevron-right me-3"),
                     width="auto",
@@ -73,14 +98,42 @@ submenu_1 = [
     # we use the collapse component to hide and reveal the navigation links
     dbc.Collapse(
         [
-            dbc.NavLink("Page 1.1", href="/page-1/1"),
-            dbc.NavLink("Page 1.2", href="/page-1/2"),
+            dbc.NavLink("Finance", href="/page-1/1"),
+            dbc.NavLink("Manufacturing", href="/page-1/2"),
+            dbc.NavLink("Information", href="/page-1/3"),
+            dbc.NavLink("Retail", href="/page-1/4"),
         ],
         id="submenu-1-collapse",
     ),
 ]
 
 submenu_2 = [
+    html.Li(
+        # use Row and Col components to postion the chevrons
+        dbc.Row(
+            [
+                dbc.Col("Historical Stock Data"),
+                dbc.Col(
+                    html.I(className="fas fa-chevron-right me-3"),
+                    width="auto",
+                ),
+            ],
+            className="my-1",
+        ),
+        style={"cursor": "pointer"},
+        id="submenu-2",
+    ),
+    # we use the collapse component to hide and reveal the navigation links
+    dbc.Collapse(
+        [
+            dbc.NavLink("Page 2.1", href="/page-2/1"),
+            dbc.NavLink("Page 2.2", href="/page-2/2"),
+        ],
+        id="submenu-2-collapse",
+    ),
+]
+
+submenu_3 = [
     html.Li(
         dbc.Row(
             [
@@ -93,14 +146,14 @@ submenu_2 = [
             className="my-1",
         ),
         style={"cursor": "pointer"},
-        id="submenu-2",
+        id="submenu-3",
     ),
     dbc.Collapse(
         [
-            dbc.NavLink("Page 2.1", href="/page-2/1"),
-            dbc.NavLink("Page 2.2", href="/page-2/2"),
+            dbc.NavLink("Page 3.1", href="/page-3/1"),
+            dbc.NavLink("Page 3.2", href="/page-3/2"),
         ],
-        id="submenu-2-collapse",
+        id="submenu-3-collapse",
     ),
 ]
 
@@ -111,7 +164,7 @@ sidebar = html.Div(
         html.P(
             "A sidebar with collapsable navigation links", className="lead"
         ),
-        dbc.Nav(submenu_1 + submenu_2, vertical=True),
+        dbc.Nav(submenu_1 + submenu_2 + submenu_3, vertical=True),
     ],
     style=SIDEBAR_STYLE,
     id="sidebar",
@@ -136,7 +189,7 @@ def set_navitem_class(is_open):
     return ""
 
 
-for i in [1, 2]:
+for i in [1, 2, 3]:
     app.callback(
         Output(f"submenu-{i}-collapse", "is_open"),
         [Input(f"submenu-{i}", "n_clicks")],
@@ -152,19 +205,169 @@ for i in [1, 2]:
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname in ["/", "/page-1/1"]:
-        return dcc.Graph(
-            id="fig2 id",
-            figure=fig2
-        )
+
+        return html.Div(children=[
+            html.H1(children='Finance', style={"text-align": "center"}),
+            html.Table(children=[
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="f1 id",
+                            figure=createFig('rtstock', 'V', 'VISA')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="f2 id",
+                            figure=createFig('rtstock', 'JPM', 'JPMoragn Chase')
+                        )
+                    )
+                ],
+                ),
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="f3 id",
+                            figure=createFig('rtstock', 'BAC', 'Bank of America')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="f4 id",
+                            figure=createFig('rtstock', 'MA', 'Mastercard ')
+                        )
+                    )
+                ],
+                ),
+            ]
+            ),
+            ]
+        ),
+
     elif pathname == "/page-1/2":
-        return dcc.Graph(
-            id="fig id",
-            figure=fig
-        )
+        return html.Div(children=[
+            html.H1(children='Manufacturing', style={"text-align": "center"}),
+            html.Table(children=[
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="m1 id",
+                            figure=createFig('rtstock', 'AAPL', 'Apple')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="m2 id",
+                            figure=createFig('rtstock', 'MSFT', 'Microsoft')
+                        )
+                    )
+                ],
+                ),
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="m3 id",
+                            figure=createFig('rtstock', 'MGPI', 'MGP Ingredients Inc')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="m4 id",
+                            figure=createFig('rtstock', 'KWR', 'Quaker Chemical Corp')
+                        )
+                    )
+                ],
+                ),
+            ]
+            ),
+            ]
+        ),
+
+    elif pathname == "/page-1/3":
+        return html.Div(children=[
+            html.H1(children='Information', style={"text-align": "center"}),
+            html.Table(children=[
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="i1 id",
+                            figure=createFig('rtstock', 'CMSA', 'Comcast')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="i2 id",
+                            figure=createFig('rtstock', 'VZ', 'Verizon')
+                        )
+                    )
+                ],
+                ),
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="i3 id",
+                            figure=createFig('rtstock', 'T', 'AT&T')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="i4 id",
+                            figure=createFig('rtstock', 'TMUS', 'T-Mobile')
+                        )
+                    )
+                ],
+                ),
+            ]
+            ),
+            ]
+        ),
+    elif pathname == "/page-1/4":
+        return html.Div(children=[
+            html.H1(children='Retail', style={"text-align": "center"}),
+            html.Table(children=[
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="i1 id",
+                            figure=createFig('rtstock', 'AMZN', 'Amazon')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="i2 id",
+                            figure=createFig('rtstock', 'WMT', 'Walmart')
+                        )
+                    )
+                ],
+                ),
+                html.Tr(children=[
+                    html.Td(
+                        dcc.Graph(
+                            id="i3 id",
+                            figure=createFig('rtstock', 'HD', 'Home Depot')
+                        )
+                    ),
+                    html.Td(
+                        dcc.Graph(
+                            id="i4 id",
+                            figure=createFig('rtstock', 'COST', 'Costco')
+                        )
+                    )
+                ],
+                ),
+            ]
+            ),
+            ]
+        ),
+
     elif pathname == "/page-2/1":
-        return html.P("Oh cool this is page 2.1")
+        return html.P("This is page 2.1")
     elif pathname == "/page-2/2":
-        return html.P("Oh cool this is page 2.2")
+        return html.P("This is page 2.2")
+    elif pathname == "/page-3/1":
+        return html.P("This is page 3.1")
+    elif pathname == "/page-3/2":
+        return html.P("This is page 3.2")
     return dbc.Jumbotron(
         [
             html.H1("404: Not Found", className="text-danger"),
